a1572f7d255721c88805aca330b8de90e8ea34d1
Gaurav Vishwakarma <gauravvishwakarma071@gmail.com>
2026-08-18 11:34:31 +0530

    chore: initialize DocuMind backend

---
+++ src/main/java/com/documind/DocumindApplication.java
@@ -0,0 +1,16 @@
+package com.documind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumindApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DocumindApplication.class, args);
+
+		System.out.println("Docu Mind");
+	}
+
+}
